327c430a7f5e21fd59633fb965ba4ae4ae71c1d2
Rafael Arellano <rafaelarellano@glampinghub.com>
2020-05-09 17:04:43 +0200

    Added modification date parameter

--- client.py
+++ client.py
@@ -25,13 +25,13 @@ class SiarClient(object):
 
     @response_json
     @siar_exceptions
-    def info_ccaa(self):
-        return requests.get(f"{API_URL}info/ccaa?ClaveAPI={self.api_key}",)
+    def info_access(self):
+        return requests.get(f"{API_URL}info/accesos?ClaveAPI={self.api_key}",)
 
     @response_json
     @siar_exceptions
-    def info_access(self):
-        return requests.get(f"{API_URL}info/accesos?ClaveAPI={self.api_key}",)
+    def info_ccaa(self):
+        return requests.get(f"{API_URL}info/ccaa?ClaveAPI={self.api_key}",)
 
     @response_json
     @siar_exceptions
@@ -43,173 +43,225 @@ class SiarClient(object):
     def info_stations(self):
         return requests.get(f"{API_URL}info/estaciones?ClaveAPI={self.api_key}",)
 
-    @response_json
-    @siar_exceptions
-    def test_bad_request(self):
-        return requests.get(f"{API_URL}info/test?ClaveAPI={self.api_key}",)
-
-    def __data_by_type(self, frequency_type, data_type, ids, start_date, end_date=None):
+    def __data_by_type(
+        self,
+        frequency_type,
+        data_type,
+        ids,
+        start_date,
+        end_date=None,
+        modification_date=None,
+    ):
         if not end_date:
             end_date = start_date
 
-        query_ids = f'&id={"&id=".join(ids)}'
+        if isinstance(ids, str):
+            ids = [ids]
 
-        url = f"{API_URL}datos/{frequency_type}/{data_type}?ClaveAPI={self.api_key}\
-                {query_ids}&fechainicial={start_date}&fechafinal={end_date}"
-        return requests.get(url)
+        endpoint = f"{API_URL}datos/{frequency_type}/{data_type}?"
+        api_key = f"claveAPI={self.api_key}"
+        ids = f'&id={"&id=".join(ids)}'
+        start_date = f"&fechaInicial={start_date}"
+        end_date = f"&fechaFinal={end_date}"
+
+        if modification_date:
+            modification_date = f"&fechaUltModificacion={modification_date}"
+        else:
+            modification_date = ""
+
+        response = requests.get(
+            f"{endpoint}{api_key}{ids}{start_date}{end_date}{modification_date}"
+        )
+
+        return response
 
     @response_json
     @siar_exceptions
-    def data_ccaa_by_hours(self, ids, start_date, end_date=None):
+    def data_ccaa_by_hours(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.hourly.value,
             self.DataType.ccaa.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_ccaa_by_day(self, ids, start_date, end_date=None):
+    def data_ccaa_by_day(self, ids, start_date, end_date=None, modification_date=None):
         response = self.__data_by_type(
             self.FrecuencyType.daily.value,
             self.DataType.ccaa.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_ccaa_by_week(self, ids, start_date, end_date=None):
+    def data_ccaa_by_week(self, ids, start_date, end_date=None, modification_date=None):
         response = self.__data_by_type(
             self.FrecuencyType.weekly.value,
             self.DataType.ccaa.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_ccaa_by_month(self, ids, start_date, end_date=None):
+    def data_ccaa_by_month(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.monthly.value,
             self.DataType.ccaa.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_provincies_by_hours(self, ids, start_date, end_date=None):
+    def data_provincies_by_hours(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.hourly.value,
             self.DataType.provincies.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_provincies_by_day(self, ids, start_date, end_date=None):
+    def data_provincies_by_day(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.daily.value,
             self.DataType.provincies.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_provincies_by_week(self, ids, start_date, end_date=None):
+    def data_provincies_by_week(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.weekly.value,
             self.DataType.provincies.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_provincies_by_month(self, ids, start_date, end_date=None):
+    def data_provincies_by_month(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.monthly.value,
             self.DataType.provincies.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_ccaa_by_month(self, ids, start_date, end_date=None):
+    def data_ccaa_by_month(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.monthly.value,
             self.DataType.ccaa.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_stations_by_hours(self, ids, start_date, end_date=None):
+    def data_stations_by_hours(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.hourly.value,
             self.DataType.stations.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_stations_by_day(self, ids, start_date, end_date=None):
+    def data_stations_by_day(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.daily.value,
             self.DataType.stations.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_stations_by_week(self, ids, start_date, end_date=None):
+    def data_stations_by_week(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.weekly.value,
             self.DataType.stations.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
 
     @response_json
     @siar_exceptions
-    def data_stations_by_month(self, ids, start_date, end_date=None):
+    def data_stations_by_month(
+        self, ids, start_date, end_date=None, modification_date=None
+    ):
         response = self.__data_by_type(
             self.FrecuencyType.monthly.value,
             self.DataType.stations.value,
             ids,
             start_date,
             end_date=None,
+            modification_date=None,
         )
         return response
